398db86b83da56b619e8e52e3930531dc0b160e6
ab <basuabhi92@gmail.com>
2025-11-22 06:19:23 +0100

    GitHub workflows

---
+++ src/test/java/io/github/absketches/plugin/concreteclazz/ClassHeaderTest.java
@@ -0,0 +1,38 @@
+package io.github.absketches.plugin.concreteclazz;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassHeaderTest {
+
+    @Test
+    void readsConcreteClassHeader() throws IOException {
+        byte[] bytes = TestUtils.buildClassBytes("com/example/Impl", "java/lang/Object", 0);
+        ClassHeader header = ClassHeader.read(new ByteArrayInputStream(bytes));
+
+        assertEquals("java/lang/Object", header.superInternalName());
+        assertFalse(header.isAbstract());
+        assertFalse(header.isInterface());
+    }
+
+    @Test
+    void detectsInterfaceAndAbstractFlags() throws IOException {
+        byte[] iface = TestUtils.buildClassBytes("com/example/Itf", "java/lang/Object", 0x0200);
+        ClassHeader ifaceHeader = ClassHeader.read(new ByteArrayInputStream(iface));
+        assertTrue(ifaceHeader.isInterface());
+
+        byte[] abs = TestUtils.buildClassBytes("com/example/Abs", "java/lang/Object", 0x0400);
+        ClassHeader abstractHeader = ClassHeader.read(new ByteArrayInputStream(abs));
+        assertTrue(abstractHeader.isAbstract());
+    }
+
+    @Test
+    void failsOnWrongMagicNumber() {
+        byte[] broken = new byte[]{0, 1, 2, 3};
+        assertThrows(IOException.class, () -> ClassHeader.read(new ByteArrayInputStream(broken)));
+    }
+}
